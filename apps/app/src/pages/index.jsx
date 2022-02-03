559ae2f5ca6f22c7e24a9c5823fe0f1540dadf52
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-02-03 13:05:40 -0800

    Add private-utils

--- apps/app/src/pages/index.jsx
+++ apps/app/src/pages/index.jsx
@@ -1,11 +1,14 @@
 import App from '../components/App'
 import {updateObjectInArrayImmutable} from 'wyx-utils'
+import {getHello} from '@wyxmono/private-utils'
 
 export default function Home() {
   return (
     <App>
-    {updateObjectInArrayImmutable([1,2,3], v => v ===1, () => 4)}
-      <p>Index Page</p>
+      <h1>Firebase deployment with yarn workspace</h1>
+      <p>This page is hosted on firebase.</p>
+      <p>This array is being updated by a public util: <b>{updateObjectInArrayImmutable([1,2,3], v => v ===1, () => 4)}</b></p>
+      <p>This hello message is from a private util which resides in the monorepo <b>{getHello()}</b></p>
     </App>
   )
 }
