c384629c7e08fce41c41a93b361d1156700e0eeb
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-02-03 12:49:48 -0800

    Add wyx-utils to app

--- apps/app/src/pages/index.jsx
+++ apps/app/src/pages/index.jsx
@@ -1,8 +1,10 @@
 import App from '../components/App'
+import {updateObjectInArrayImmutable} from 'wyx-utils'
 
 export default function Home() {
   return (
     <App>
+    {updateObjectInArrayImmutable([1,2,3], v => v ===1, () => 4)}
       <p>Index Page</p>
     </App>
   )
